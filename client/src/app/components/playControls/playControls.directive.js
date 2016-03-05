9b90e4be01d334f8c9b35687f5dd47e194b0d796
Jeff Fohl <jeff@fohl.com>
2016-03-05 15:41:43 -0800

    Ability to scrub through a recording.

--- client/src/app/components/playControls/playControls.directive.js
+++ client/src/app/components/playControls/playControls.directive.js
@@ -1,45 +1,76 @@
 (function() {
   'use strict';
-
   angular
     .module('leapgesture')
     .directive('playControls', playControlsDirective);
-
-
   function playControlsDirective($log, leapController) {
-
     return {
-      restrict : 'AE',
+      restrict : 'E',
       replace : true,
       templateUrl : 'app/components/playControls/playControls.html',
       link : function(scope, element, attrs) {
 
-        var progressBar = angular.element(element.find('.playhead')[0]);
-
-        var watchers = {};
-
-        leapController.controller.on('frame', function(){
-          if (leapController.player && leapController.player.state === "playing") {
-            if (scope.main.player && scope.main.player.recording) {
-              var percentage = ((scope.main.player.recording.frameIndex / (scope.main.player.recording.frameCount - 1)) * 100) + "%";
-              progressBar.css({"width" : percentage});
-            } else {
-              progressBar.css({"width" : "0%"});
-            }
-          }
-        });
-
-        scope.$on("$destroy", function(){
-          angular.forEach(watchers, function(watcher){
-            watcher();
-          });
-        });
-
-
       }
     };
-
   }
+})();
 
+
+(function() {
+  'use strict';
+  angular
+    .module('leapgesture')
+    .directive('recording', recordingDirective);
+  function recordingDirective(leapController, $document) {
+    return {
+      restrict : 'E',
+      replace : false,
+      link : function(scope, element, attrs) {
+        element.on('mousedown', function(mouseDownEvent){
+          setNewFrameIndex(mouseDownEvent);
+          $document.on('mousemove', function(mouseMoveEvent){
+            setNewFrameIndex(mouseMoveEvent);
+          });
+        });
+        $document.on('mouseup', function(){
+          $document.off('mousemove');
+        });
+
+        function setNewFrameIndex(event) {
+          if (leapController.player && leapController.player.recording) {
+            var percentage = event.offsetX / element.width();
+            var newFrameIndex = Math.floor(leapController.player.recording.frameCount * percentage);
+            leapController.player.setFrameIndex(newFrameIndex);
+          }
+        }
+      }
+    };
+  }
+})();
+
+(function() {
+  'use strict';
+  angular
+    .module('leapgesture')
+    .directive('playhead', playheadDirective);
+  function playheadDirective(leapController) {
+    return {
+      restrict : 'E',
+      replace : false,
+      link : function(scope, element, attrs) {
+        leapController.controller.on('frame', setPlayhead);
+        function setPlayhead() {
+          if (leapController.player) {
+            if (scope.main.player && scope.main.player.recording) {
+              var percentage = ((scope.main.player.recording.frameIndex / (scope.main.player.recording.frameCount - 1)) * 100) + "%";
+              element.css({"width" : percentage});
+            } else {
+              element.css({"width" : "0%"});
+            }
+          }
+        }
+      }
+    };
+  }
 })();
 
